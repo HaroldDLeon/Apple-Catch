f537baae262e784928ef258909fd69c219be4fc2
HaroldDLeon <harolddleon@users.noreply.github.com>
2018-05-05 13:38:08 -0400

    Modified basket's base rectangle

--- src/Basket.java
+++ src/Basket.java
@@ -7,7 +7,7 @@ public class Basket extends Rect {
 	Image basket_img = Toolkit.getDefaultToolkit().getImage("../assets/basket.png");
 	
 	public Basket(int x, int y) {
-		super(x,y,60,20);
+		super(x, y, 100, 90);
 	}
 	
 	public void draw(Graphics g) {
